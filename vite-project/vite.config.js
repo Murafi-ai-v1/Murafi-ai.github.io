5c93d324004b16038f06ab58c849ea1b34f28f1b
Shakir-Mushtaq <229188219+Shakir-Mushtaq@users.noreply.github.com>
2025-09-06 07:52:36 +0000

    Final Version

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Murafi-ai.github.io/',  // 👈 must match your repo name exactly
+  base: './',
 })
